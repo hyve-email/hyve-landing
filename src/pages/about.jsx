52e4166c34d28e297f171b7e9fbd0ec489172065
david-castaneda <dcast188@fiu.edu>
2019-07-28 00:41:16 -0500

    added policy

---
+++ src/pages/about.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+const About =() => (
+    <>
+    <h1>Hyve’s privacy policy </h1>
+    <p>This privacy policy sets forth Hyve’s information gathering and dissemination practices regarding personal information collected by the company directly from individuals (you) in connection with the website located at https://Hyve.email and related URLs (the website).</p>
+    <h1>What We Collect</h1>
+    <p>Hyve may collect various types of personal information from you in connection with your use of the website. Such data may include identifying information and contact information (names and email addresses) and IP addresses.</p>
+    <h1>How We Use Collected Information</h1>
+    <h2>Contact Information</h2>
+    <p>We may use personal information collected in connection with the website to respond to your inquiry, to provide services to you, and for other purposes related to our business. You may choose to opt out of receiving communications from us. If you do not wish to receive future communications, please send an email to info@Hyve.email.</p>
+    <h2>IP addresses</h2>
+    <p>We may use your unique IP address to help diagnose problems with our servers or the website.</p>
+    <h2>Cookies and analytics</h2>
+    <p>We use web analytics tools to evaluate the effectiveness of our website and may use cookies for the same purpose.</p>
+    <h2>Disclosure of information</h2>
+    <p>Hyve may disclose to third parties information we collect from you in connection with the Website (i) as necessary to provide services requested by you, (ii) to our subcontractors, service providers and partners in connection with the delivery of services to you (provided they are under a duty to maintain confidentiality), and (iii) to comply with requests from law enforcement officials, government agencies, or otherwise as required by law. In addition, we may transfer information we collect from you to a third party in connection with a merger or sale of our business or assets, in which case the transferee will be permitted to keep and use such information subject to the terms of this privacy policy.</p>
+    <h2>Linked sites</h2>
+    <p>The Hyve website may at times contain links to third-party websites. Hyve is not responsible for the privacy policies or content on those sites.</p>
+    <h2>Updates</h2>
+    <p>Hyve may update this privacy policy from time to time, and such changes shall become effective promptly after they are posted. Hyve encourages you to periodically review this privacy policy to be informed of any changes. </p>
+    <h3><b>This Privacy Policy was last updated on July 27, 2019.</b></h3>
+    <b>More information</b>
+
+    <p>For more information, call Hyve at 813-966-7396 or email info@Hyve.email.</p>
+
+    </>
+)
+
+export default About
